d6e303febe8049fac7bf7efe9d083ef03fc92665
alejandro.cordoba <alejo.artia@gmail.com>
2023-03-01 17:39:49 +0100

    Add: endpoint to receive CSV file

--- app/routes/app.py
+++ app/routes/app.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, File
+import pandas as pd
+import io
 
 # Create an API router for the user endpoint
 dashboard = APIRouter()
 
 @dashboard.get('/')
-async def home():
+async def home() -> dict:
     """
     This is the home page, it just shows a message
     """
-    return "Welcome Data-vision"
+    return {"message": "Data-vision"}
 
+@dashboard.post('/upload')
+async def upload_file(file: bytes = File(...)) -> dict:
+    """
+    This method allows to upload files
+    """
+    decoded_file: str = file.decode('utf-8')
+    file_reader: pd.DataFrame = pd.read_csv(io.StringIO(decoded_file))
+    summary_stats: pd.DataFrame = file_reader.describe()
+    return summary_stats.to_dict()
