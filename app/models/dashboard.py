954ee61fd652f15c602873bc09065a9628229084
alejandro <acordoba@ext.laligatech.com>
2023-03-02 19:52:44 +0100

    Add: model dashboard to migrate

--- app/models/dashboard.py
+++ app/models/dashboard.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Date
+
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 Base = declarative_base()
 
 
-class Review(Base):
-    __tablename__ = "reviews"
+class Dashboard(Base):
+    __tablename__ = "Dashboard"
     id = Column(Integer, primary_key=True, index=True)
     review_time = Column(Integer)
     team = Column(String)
