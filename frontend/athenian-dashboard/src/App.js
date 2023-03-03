be59053a296591b88c8a6ea52cd0254dfd1b727d
alejandro <acordoba@ext.laligatech.com>
2023-03-03 01:58:03 +0100

    Add: Frontend dashboard features

--- frontend/athenian-dashboard/src/App.js
+++ frontend/athenian-dashboard/src/App.js
@@ -24,16 +24,63 @@ function App() {
     ],
   });
 
+  const [fileListData, setFileListData] = useState([]);
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = (event) => {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      console.error("No file selected");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    fetch("http://0.0.0.0:8000/app/v1/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("File upload successful: ", data);
+      })
+      .catch((error) => {
+        console.error("Error uploading file: ", error);
+      });
+  };
+
+  const handleAnalysisFile = (id) => {
+    fetch("http://0.0.0.0:8000/app/v1/analysis_file", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Analysis file successful: ", data);
+      })
+      .catch((error) => {
+        console.error("Error analyzing file: ", error);
+      });
+  };
+
   useEffect(() => {
-    fetch("http://0.0.0.0:8000/app/v1/user-stats")
+    fetch("http://0.0.0.0:8000/app/v1/review-stats")
       .then((response) => response.json())
       .then((data) => {
         setUserData({
-          labels: data.map((data) => data.year),
+          labels: data.map((data) => data.name),
           datasets: [
             {
               label: "Users Gained",
-              data: data.map((data) => data.userGain),
+              data: data.map((data) => data.mean_review_time),
               backgroundColor: [
                 "rgba(75,192,192,1)",
                 "#ecf0f1",
@@ -50,21 +97,105 @@ function App() {
       .catch((error) => {
         console.error("Error fetching user data: ", error);
       });
+
+    fetch("http://0.0.0.0:8000/app/v1/filelist")
+      .then((response) => response.json())
+      .then((data) => {
+        setFileListData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching file list data: ", error);
+      });
   }, []);
 
   // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
 
-  return (
+
+return (
     <div className="App">
-      <div style={{ width: 700 }}>
+      <h1>Data-Vision Athenian Dashboard </h1>
+      <form
+        onSubmit={handleFileUpload}
+        style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <div style={{ marginBottom: "1rem" }}>
+          <input
+            type="file"
+            onChange={handleFileSelect}
+            style={{ display: "none" }}
+            id="file-upload"
+          />
+          <label
+            htmlFor="file-upload"
+            style={{
+              backgroundColor: "#2a71d0",
+              color: "white",
+              padding: "0.5rem 1rem",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            Choose a file
+          </label>
+        </div>
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#50AF95",
+            color: "white",
+            padding: "0.5rem 1rem",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          Upload
+        </button>
+      </form>
+        <div style={{ width: 700 }}>
+        <table style={{ border: "1px solid black", borderCollapse: "collapse", margin: "20px" }}>
+          <thead>
+            <tr>
+              <th style={{ border: "1px solid black", padding: "0.5rem" }}>ID</th>
+              <th style={{ border: "1px solid black", padding: "0.5rem" }}>Date</th>
+              <th style={{ border: "1px solid black", padding: "0.5rem" }}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {fileListData.map((file) => (
+              <tr key={file.id}>
+                <td style={{ border: "1px solid black", padding: "0.5rem" }}>{file.id}</td>
+                <td style={{ border: "1px solid black", padding: "0.5rem" }}>{file.date}</td>
+                <td style={{ border: "1px solid black", padding: "0.5rem" }}>
+                  <button
+                    onClick={() => handleAnalysisFile(file.id)}
+                    style={{
+                      backgroundColor: "#f3ba2f",
+                      color: "white",
+                      padding: "0.5rem 1rem",
+                      borderRadius: "5px",
+                      border: "none",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Analyze
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div style={{ width: 350 }}>
         <BarChart chartData={userData} />
       </div>
-      <div style={{ width: 700 }}>
+      <div style={{ width: 350 }}>
         <LineChart chartData={userData} />
       </div>
-      <div style={{ width: 700 }}>
+      <div style={{ width: 350 }}>
         <PieChart chartData={userData} />
       </div>
+
     </div>
   );
 }
