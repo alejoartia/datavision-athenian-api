f3648a952b1c8281032f4b9432ad5024f986a13c
alejandro <acordoba@ext.laligatech.com>
2023-03-04 13:45:45 +0100

    refactor: Non-functional validators

--- app/schemas/dashboard.py
+++ app/schemas/dashboard.py
@@ -9,15 +9,6 @@ class Dashboard(BaseModel):
     review_date: date
     merge_time: Annotated[int, "Number of seconds"]
 
-    # This is because in a normal distribution of review times,
-    # the mean is typically less than the median. If the mean is greater than or equal to the median,
-    # it suggests that the distribution is skewed
-    @validator("review_time", "merge_time")
-    def check_time_range(self, v):
-        if v < 0 or v > 86400:
-            raise ValueError("Time must be between 0 and 86400 seconds")
-        return v
-
     class Config:
         orm_mode = True
 
@@ -46,15 +37,6 @@ class TeamStats(BaseModel):
     median_merge_time: float
     mode_merge_time: float
     created_date: date
-
-    # This is because in a normal distribution of review times,
-    # the mean is typically less than the median. If the mean is greater than or equal to the median,
-    # it suggests that the distribution is skewed
-    @root_validator
-    def check_time_order(self, values):
-        if values.get("mean_review_time") >= values.get("median_review_time"):
-            raise ValueError("Mean review time must be less than median review time")
-        return values
 
     class Config:
         orm_mode = True
