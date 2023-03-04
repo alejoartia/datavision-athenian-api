49e7e7cd91cd7b0424cc763809b89f5fc4406a22
alejandro <acordoba@ext.laligatech.com>
2023-03-04 10:40:59 +0100

    add: validators and type hints missed

--- app/schemas/dashboard.py
+++ app/schemas/dashboard.py
@@ -1,13 +1,22 @@
-from pydantic import BaseModel
 from datetime import date
-from typing import List
+from typing import Annotated
+from pydantic import BaseModel, validator, root_validator
 
 
 class Dashboard(BaseModel):
-    review_time: int
-    team: str
-    date: date
-    merge_time: int
+    review_time: Annotated[int, "Number of seconds"]
+    team_name: str
+    review_date: date
+    merge_time: Annotated[int, "Number of seconds"]
+
+    # This is because in a normal distribution of review times,
+    # the mean is typically less than the median. If the mean is greater than or equal to the median,
+    # it suggests that the distribution is skewed
+    @validator("review_time", "merge_time")
+    def check_time_range(self, v):
+        if v < 0 or v > 86400:
+            raise ValueError("Time must be between 0 and 86400 seconds")
+        return v
 
     class Config:
         orm_mode = True
@@ -28,15 +37,24 @@ class QueriesAnalyzed(BaseModel):
 
 
 class TeamStats(BaseModel):
-    id: int
-    name: str
+    team_id: int
+    team_name: str
     mean_review_time: float
     median_review_time: float
     mode_review_time: float
     mean_merge_time: float
     median_merge_time: float
     mode_merge_time: float
-    date_created: date
+    created_date: date
+
+    # This is because in a normal distribution of review times,
+    # the mean is typically less than the median. If the mean is greater than or equal to the median,
+    # it suggests that the distribution is skewed
+    @root_validator
+    def check_time_order(self, values):
+        if values.get("mean_review_time") >= values.get("median_review_time"):
+            raise ValueError("Mean review time must be less than median review time")
+        return values
 
     class Config:
         orm_mode = True
