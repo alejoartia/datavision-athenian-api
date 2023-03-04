acaebf4bfc775c24a5b01e90a79274d8d38abb84
alejandro <acordoba@ext.laligatech.com>
2023-03-04 10:43:17 +0100

    add: adding some PEP8 practices

--- app/index.py
+++ app/index.py
@@ -5,21 +5,27 @@ from fastapi_sqlalchemy import DBSessionMiddleware
 import os
 
 
+# Create the FastAPI application
 app = FastAPI()
+
+# Add middleware for database sessions
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 # Include the user router in the application
 app.include_router(dashboard, prefix='/app/v1')
 
-# Allow all origins for CORS
-origins = ["*"]
+# Define the CORS settings as constants
+ALLOWED_ORIGINS = ["*"]
+ALLOWED_METHODS = ["*"]
+ALLOWED_HEADERS = ["*"]
+ALLOW_CREDENTIALS = True
 
 # Add CORS middleware to the application with the allowed origins, allow credentials,
 # methods, and headers.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=ALLOW_CREDENTIALS,
+    allow_methods=ALLOWED_METHODS,
+    allow_headers=ALLOWED_HEADERS,
 )
