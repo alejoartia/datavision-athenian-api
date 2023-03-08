e5f37663c540a4ef9867bc04c1b7c2636ba0035d
alejandro <acordoba@ext.laligatech.com>
2023-03-08 17:27:30 +0100

    Refactor: disaggregation of the code, abstraction refactor

--- app/analysis/dashboard_repository.py
+++ app/analysis/dashboard_repository.py
@@ -1,5 +1,8 @@
-from app.models.dashboard import Dashboard, FileCsv, QueriesAnalyzed
+from typing import List
+from app.models.dashboard import Dashboard, FileCsv, QueriesAnalyzed, StatsId, TeamStats
 from fastapi_sqlalchemy import db
+from sqlalchemy import desc, func
+from sqlalchemy.sql.expression import not_
 
 
 class DashboardRepository:
@@ -23,3 +26,40 @@ class DashboardRepository:
     def create_queries_analyzed(queries_analyzed: QueriesAnalyzed):
         db.session.add(queries_analyzed)
         db.session.commit()
+
+    @staticmethod
+    def get_last_query_number() -> int:
+        return db.session.query(func.max(QueriesAnalyzed.query_number)).scalar()
+
+    @staticmethod
+    def get_dashboard_data_by_query_number(last_query_number: int) -> Dashboard:
+        return db.session.query(Dashboard).join(FileCsv).filter(
+            QueriesAnalyzed.query_number == last_query_number
+        ).all()
+
+    @staticmethod
+    def get_files() -> List[FileCsv]:
+        return db.session.query(FileCsv).all()
+
+    @staticmethod
+    def get_max_query_number() -> int:
+        return db.session.query(func.max(StatsId.query_number)).scalar()
+
+    @staticmethod
+    def add_stats_id(stats_id: StatsId) -> None:
+        db.session.add(stats_id)
+        db.session.commit()
+
+    @staticmethod
+    def add_team_stats(teamstats: TeamStats) -> None:
+        db.session.add(teamstats)
+        db.session.commit()
+
+    @staticmethod
+    def get_analysis_data() -> List[StatsId]:
+        return db.session.query(StatsId).filter(not_(StatsId.query_number.is_(None))).all()
+
+    @staticmethod
+    def get_team_stats_by_id(id: int) -> List[TeamStats]:
+        return db.session.query(TeamStats).filter(TeamStats.stats_id == id).all()
+
--- app/analysis/dashboard_service.py
+++ app/analysis/dashboard_service.py
@@ -1,7 +1,8 @@
 import io
 import pandas as pd
+from typing import Any, List, Dict
 from app.analysis.dashboard_repository import DashboardRepository
-from app.models.dashboard import Dashboard, FileCsv, QueriesAnalyzed
+from app.models.dashboard import Dashboard, FileCsv, QueriesAnalyzed, StatsId, TeamStats
 
 
 class DashboardService:
@@ -41,3 +42,147 @@ class DashboardService:
         )
         self.dashboard_repository.create_queries_analyzed(queries_analyzed)
         return 'The query has been saved'
+
+    def get_review_stats(self) -> list[dict[str, Any]]:
+        last_query_number = self.dashboard_repository.get_last_query_number()
+
+        if last_query_number is None:
+            return []
+
+        dashboard_data: List[Dashboard] = self.dashboard_repository.get_dashboard_data_by_query_number(
+            last_query_number)
+
+        if dashboard_data is None:
+            return []
+
+        data = [(d.id, d.review_time, d.team, d.date, d.merge_time) for d in dashboard_data]
+        df = pd.DataFrame(data, columns=['id', 'review_time', 'team', 'date', 'merge_time'])
+
+        if df.empty:
+            return []
+
+        grouped_data = df.groupby("team")
+        mean_review_time = grouped_data["review_time"].mean()
+        median_review_time = grouped_data["review_time"].median()
+        mode_review_time = grouped_data["review_time"].agg(pd.Series.mode).astype(float)
+
+        mean_merge_time = grouped_data["merge_time"].mean()
+        median_merge_time = grouped_data["merge_time"].median()
+        mode_merge_time = grouped_data["merge_time"].agg(pd.Series.mode).astype(float)
+
+        review_stats = []
+        for team in grouped_data.groups:
+            team_dict = {
+                "name": team,
+                "mean_review_time": mean_review_time[team],
+                "median_review_time": median_review_time[team],
+                "mode_review_time": mode_review_time[team].tolist(),
+                "mean_merge_time": mean_merge_time[team],
+                "median_merge_time": median_merge_time[team],
+                "mode_merge_time": mode_merge_time[team].tolist(),
+            }
+            review_stats.append(team_dict)
+
+        return review_stats
+
+    def get_file_list(self) -> list[dict[str, Any]]:
+        dashboard_data = self.dashboard_repository.get_files()
+
+        if dashboard_data is None:
+            return []
+
+        data = [(d.id, d.time_created) for d in dashboard_data]
+        df = pd.DataFrame(data, columns=['_id', 'time_created'])
+
+        if df.empty:
+            return []
+
+        data_dict = df.to_dict('records')
+        user_stats = [{"id": d['_id'], "date": d['time_created'].strftime('%Y-%m-%d %H:%M')} for d in data_dict]
+
+        return user_stats
+
+    def save_stats(self) -> list[dict[str, Any]]:
+        review_stats_data = self.get_review_stats()
+
+        if not review_stats_data:
+            return []
+
+        df = pd.DataFrame(review_stats_data)
+
+        max_query_number = self.dashboard_repository.get_max_query_number()
+
+        if max_query_number is None:
+            max_query_number = 1
+        else:
+            max_query_number += 1
+
+        stats_id = StatsId(query_number=max_query_number)
+        self.dashboard_repository.add_stats_id(stats_id)
+
+        for row in df.itertuples():
+            teamstats = TeamStats(
+                name=row.name,
+                mean_review_time=row.mean_review_time,
+                median_review_time=row.median_review_time,
+                mode_review_time=row.mode_review_time,
+                mean_merge_time=row.mean_merge_time,
+                median_merge_time=row.median_merge_time,
+                mode_merge_time=row.mode_merge_time,
+                stats_id=stats_id.query_number
+            )
+            self.dashboard_repository.add_team_stats(teamstats)
+
+        return review_stats_data
+
+    def get_analysis_data(self) -> List[Dict[str, str]]:
+        analysis_data = self.dashboard_repository.get_analysis_data()
+
+        if analysis_data is None:
+            # If there is no analysis data, return an empty list
+            return []
+
+        stats = [
+            {"query_number": row.query_number,
+             "date": str(row.time_created.strftime('%Y-%m-%d %H:%M'))} for row in analysis_data
+        ]
+        return stats
+
+    def get_team_stats_by_id(self, id: int) -> List[Dict[str, Any]]:
+        team_stats = self.dashboard_repository.get_team_stats_by_id(id)
+
+        if team_stats is None:
+            # If there is no data for the specified ID, return an empty list
+            return []
+
+        data = [(d.id,
+                 d.name,
+                 d.mean_review_time,
+                 d.median_review_time,
+                 d.mode_review_time,
+                 d.mean_merge_time,
+                 d.median_merge_time,
+                 d.mode_merge_time) for d in team_stats]
+
+        df = pd.DataFrame(data, columns=['id',
+                                         'name',
+                                         'mean_review_time',
+                                         'median_review_time',
+                                         'mode_review_time',
+                                         'mean_merge_time',
+                                         'median_merge_time',
+                                         'mode_merge_time'])
+
+        df_dict = df.to_dict(orient='records')
+
+        # Create a new list of dictionaries with the desired keys
+        stats_list = [{"id": d['id'],
+                       "name": d['name'],
+                       "mean_review_time": d['mean_review_time'],
+                       "median_review_time": d['median_review_time'],
+                       "mode_review_time": d['mode_review_time'],
+                       "mean_merge_time": d['mean_merge_time'],
+                       "median_merge_time": d['median_merge_time'],
+                       "mode_merge_time": d['mode_merge_time']} for d in df_dict]
+
+        return stats_list
